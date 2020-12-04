809212df3826abf235319dcd50b113a4cb99afc1
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-04 16:42:55 -0500

    app, header and footer content styled

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import React from "react";
 import Header from "./components/nav/header";
 import Footer from "./components/nav/footer";
 import DropDown from "./components/nav/dropdown";
-import Easy from "./components/cards/easy";
-import Normal from "./components/cards/easy";
-import Hard from "./components/cards/easy";
-import XtraHard from "./components/cards/easy";
+import Game from "./components/game"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Game />
       <Footer />
     </div>
   );
---
+++ src/components/game.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Easy from "./cards/easy";
+import Normal from "./cards/easy";
+import Hard from "./cards/easy";
+import XtraHard from "./cards/easy";
+
+function Game() {
+
+  return (
+    <div class="game">
+
+    </div>
+  )
+
+}
+
+export default Game;
