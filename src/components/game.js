17502a1c992970b4c15137c720528d4679c54e24
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-05 13:16:36 -0500

    switched card info json to export component, add flipped attribute

--- src/components/game.js
+++ src/components/game.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Card from "./cards/card";
+import Card from "./card";
 
 class Game extends React.Component {
-  
+
 }
 
 export default Game;
---
+++ src/components/images.js
@@ -0,0 +1,184 @@
+import React from 'react';
+
+export let images = [
+  {
+    id: 1,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/aloe-1.png",
+    flipped: false
+  },
+  {
+    id: 2,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/anthurium.png",
+    flipped: false
+  },
+  {
+    id: 3,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/bamboo.png",
+    flipped: false
+  },
+  {
+    id: 4,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/bonsai.png",
+    flipped: false
+  },
+  {
+    id: 5,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/bouquet-of-daisys.png",
+    flipped: false
+  },
+  {
+    id: 6,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/bush.png",
+    flipped: false
+  },
+  {
+    id: 7,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/cactus-1.png",
+    flipped: false
+  },
+  {
+    id: 8,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/cactus-2.png",
+    flipped: false
+  },
+  {
+    id: 9,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/calla.png",
+    flipped: false
+  },
+  {
+    id: 10,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/camomile.png",
+    flipped: false
+  },
+  {
+    id: 11,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/dandelion.png",
+    flipped: false
+  },
+  {
+    id: 12,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/dry-tree.png",
+    flipped: false
+  },
+  {
+    id: 13,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/fennel.png",
+    flipped: false
+  },
+  {
+    id: 14,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/flower-3.png",
+    flipped: false
+  },
+  {
+    id: 15,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/flower-4.png",
+    flipped: false
+  },
+  {
+    id: 16,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/flower-6.png",
+    flipped: false
+  },
+  {
+    id: 17,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/flower.png",
+    flipped: false
+  },
+  {
+    id: 18,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/gerbera.png",
+    flipped: false
+  },
+  {
+    id: 19,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/hareball.png",
+    flipped: false
+  },
+  {
+    id: 20,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/leaves.png",
+    flipped: false
+  },
+  {
+    id: 21,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/lily.png",
+    flipped: false
+  },
+  {
+    id: 22,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/lotus.png",
+    flipped: false
+  },
+  {
+    id: 23,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/oak.png",
+    flipped: false
+  },
+  {
+    id: 24,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/palm-tree.png",
+    flipped: false
+  },
+  {
+    id: 25,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/peony.png",
+    flipped: false
+  },
+  {
+    id: 26,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/plant.png",
+    flipped: false
+  },
+  {
+    id: 27,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/poppy.png",
+    flipped: false
+  },
+  {
+    id: 28,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/reed.png",
+    flipped: false
+  },
+  {
+    id: 29,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/rose-1.png",
+    flipped: false
+  },
+  {
+    id: 30,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/sprout-1.png",
+    flipped: false
+  },
+  {
+    id: 31,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/sprout.png",
+    flipped: false
+  },
+  {
+    id: 32,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/sprouting-bean.png",
+    flipped: false
+  },
+  {
+    id: 33,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/sunflower-1.png",
+    flipped: false
+  },
+  {
+    id: 34,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/tree.png",
+    flipped: false
+  },
+  {
+    id: 35,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/tulip.png",
+    flipped: false
+  },
+  {
+    id: 36,
+    img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/wheat.png",
+    flipped: false
+  }
+]
