f9270868641de2a8fcbc24516b0e99dbaf2f6a96
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-07 15:24:26 -0500

    added name and score to header component

--- src/components/nav/header.js
+++ src/components/nav/header.js
@@ -2,9 +2,17 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
+    const { name, score } = this.props;
     return ( 
       <div className="nav">
-      <p> Memory Match Game </p>
+        <div className="nav-left">
+          <p> Memory Match Game </p>
+        </div>
+
+        <div className="nav-right">
+          <div className="user-name"> {name} </div>
+          <div className="user-score"> {score} </div>
+        </div>
       </div>)
   }
 }
