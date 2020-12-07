dabc59095cf4a43a8eeb9485341d7b6596c90303
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-07 15:19:24 -0500

    change and click functions added to login

--- src/components/login.js
+++ src/components/login.js
@@ -3,6 +3,15 @@ import React from "react";
 class Login extends React.Component {
   state = {}
 
+  handleChange = (e) => {
+    this.setState({name: e.target.value});
+  };
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.props.name(this.state.name, false);
+  };
+
   render() {
     return null
   }
