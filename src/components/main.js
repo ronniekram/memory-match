42736fdb0a70b007359c030a7ecff1c4f6ef037d
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-07 15:13:46 -0500

    main component filled out

--- src/components/main.js
+++ src/components/main.js
@@ -6,9 +6,39 @@ import Header from "./components/nav/header";
 import Footer from "./components/nav/footer";
 
 class Main extends React.Component {
+  state = {
+    showLogin: true,
+    showEndGame: false,
+    name: "",
+    score: 0
+  };
+
+  handleLogin = (name, boolean) {
+    this.setState({name: name, showLogin: boolean})
+  };
+
+  handleEndGame = (boolean) {
+    if (boolean) {
+      this.setState({showEndGame: boolean, score: this.state.score + 1});
+    } else {
+      this.setState({showEndGame: boolean});
+    }
+  };
+
+
 
   render() {
-    return null
+    const {showLogin, name, score, showEndGame} = this.state;
+
+    return (
+      <div>
+        {showLogin ? <Login name={this.handleLogin}/> : null}
+        {showEndGame ? <GameOver newGame={this.handleEndGame} /> : null}
+        <Header name={name} score={score} />
+        <Game endGame={this.handleEndGame} />
+        <Footer />
+      </div>
+    )
   }
 }
 
