8f00c9ff5b28a34d9b321c20a2e0c41b397022fb
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-10 17:59:21 -0500

    added handle click to game

--- src/components/game.js
+++ src/components/game.js
@@ -4,13 +4,25 @@ import _ from 'lodash';
 import images from './images';
 
 class Game extends React.Component {
+  choices = []
+
+  handleClick = event => {
+    let pick = event.target;
+    if (pick.getAttribute("check") === "found") {
+      return;
+    }
+  }
+
+
+
+
   render() {
     let cards = _.sampleSize(images, 8)
     return (
       <div className="game">
-        {images.map(img => {
+        {cards.map(card => {
           return(
-            <div className="card-img" key={img.id} style={ { background: `url(${img.img_url})`} } onClick={this.handleClick}> 
+            <div className="card-blank" key={card.id} style={ { background: `url(${card.img_url})`} } onClick={this.handleClick}> 
 
             </div>
           )
