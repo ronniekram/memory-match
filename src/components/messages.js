1e230ae785db0e23bb4e1442a63686a8a2e861e8
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-10 18:41:12 -0500

    filled out messages component

---
+++ src/components/messages.js
@@ -0,0 +1,6 @@
+
+export let messages = [
+  {title:  "Well Done!", message: "Do you want to play again?"},
+  {title:  "Nice Work!", message: "Bet you can't do that again!"},
+  {title:  "You Did It!", message: "You have the memory of an elephant!"}
+];
