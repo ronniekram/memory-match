224a759b2ed00094419e1792a5fd78ffdfc1ec58
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-10 18:36:58 -0500

    game over display

--- src/components/gameover.js
+++ src/components/gameover.js
@@ -13,9 +13,9 @@ class GameOver extends React.Component() {
     return (
       <div className="game-over">
         <div className="message">
-          <h2></h2>
-          <p></p>
-          <button></button>
+          <h2> {messages[0].title} </h2>
+          <p> {messages[1].message} </p>
+          <button onClick={this.handleClick}>Play Again</button>
         </div>
       </div>
     )
