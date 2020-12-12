05a5745c471dfc4a87a74eafec9e559fefb38416
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-12 13:47:41 -0500

    click working again, but card back still not displaying

--- src/components/images.js
+++ src/components/images.js
@@ -5,217 +5,217 @@ export let images = [
     id: 1,
     name: "aloe",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/aloe-1.png",
-    check: false
+    check: "false"
   },
   {
     id: 2,
     name: "anthurium",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/anthurium.png",
-    check: false
+    check: "false"
   },
   {
     id: 3,
     name: "bamboo",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/bamboo.png",
-    check: false
+    check: "false"
   },
   {
     id: 4,
     name: "bonsai",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/bonsai.png",
-    check: false
+    check: "false"
   },
   {
     id: 5,
     name: "bouquet",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/bouquet-of-daisys.png",
-    check: false
+    check: "false"
   },
   {
     id: 6,
     name: "bush",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/bush.png",
-    check: false
+    check: "false"
   },
   {
     id: 7,
     name: "cactus-1",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/cactus-1.png",
-    check: false
+    check: "false"
   },
   {
     id: 8,
     name: "cactus-2",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/cactus-2.png",
-    check: false
+    check: "false"
   },
   {
     id: 9,
     name: "calla",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/calla.png",
-    check: false
+    check: "false"
   },
   {
     id: 10,
     name: "camomile",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/camomile.png",
-    check: false
+    check: "false"
   },
   {
     id: 11,
     name: "dandelion",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/dandelion.png",
-    check: false
+    check: "false"
   },
   {
     id: 12,
     name: "dry-tree",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/dry-tree.png",
-    check: false
+    check: "false"
   },
   {
     id: 13,
     name: "fennel",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/fennel.png",
-    check: false
+    check: "false"
   },
   {
     id: 14,
     name: "flower-3",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/flower-3.png",
-    check: false
+    check: "false"
   },
   {
     id: 15,
     name: "flower-4",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/flower-4.png",
-    check: false
+    check: "false"
   },
   {
     id: 16,
     name: "flower-6",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/flower-6.png",
-    check: false
+    check: "false"
   },
   {
     id: 17,
     name: "flower",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/flower.png",
-    check: false
+    check: "false"
   },
   {
     id: 18,
     name: "gerbera",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/gerbera.png",
-    check: false
+    check: "false"
   },
   {
     id: 19,
     name: "harebell",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/harebell.png",
-    check: false
+    check: "false"
   },
   {
     id: 20,
     name: "leaves",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/leaves.png",
-    check: false
+    check: "false"
   },
   {
     id: 21,
     name: "lily",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/lily.png",
-    check: false
+    check: "false"
   },
   {
     id: 22,
     name: "lotus",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/lotus.png",
-    check: false
+    check: "false"
   },
   {
     id: 23,
     name: "oak",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/oak.png",
-    check: false
+    check: "false"
   },
   {
     id: 24,
     name: "palm",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/palm-tree.png",
-    check: false
+    check: "false"
   },
   {
     id: 25,
     name: "peony",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/peony.png",
-    check: false
+    check: "false"
   },
   {
     id: 26,
     name: "plant",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/plant.png",
-    check: false
+    check: "false"
   },
   {
     id: 27,
     name: "poppy",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/poppy.png",
-    check: false
+    check: "false"
   },
   {
     id: 28,
     name: "reed",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/reed.png",
-    check: false
+    check: "false"
   },
   {
     id: 29,
     name: "rose-1",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/rose-1.png",
-    check: false
+    check: "false"
   },
   {
     id: 30,
     name: "sprout-1",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/sprout-1.png",
-    check: false
+    check: "false"
   },
   {
     id: 31,
     name: "sprout",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/sprout.png",
-    check: false
+    check: "false"
   },
   {
     id: 32,
     name: "sprouting-bean",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/sprouting-bean.png",
-    check: false
+    check: "false"
   },
   {
     id: 33,
     name: "sunflower-1",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/sunflower-1.png",
-    check: false
+    check: "false"
   },
   {
     id: 34,
     name: "tree",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/tree.png",
-    check: false
+    check: "false"
   },
   {
     id: 35,
     name: "tulip",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/tulip.png",
-    check: false
+    check: "false"
   },
   {
     id: 36,
     name: "wheat",
     img_url: "https://raw.githubusercontent.com/ronniekram/memory-match/main/src/images/wheat.png",
-    check: false
+    check: "false"
   }
 ]
 
