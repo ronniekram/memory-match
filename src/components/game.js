203ef023ea95a992c370f4553be033004d948f78
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-22 17:48:39 -0500

    fixed card match error, had not added id to div

--- src/components/game.js
+++ src/components/game.js
@@ -44,7 +44,7 @@ class Game extends React.Component {
   };
 
   checkName = (character1, character2) => {
-    if (character1.getAttribute("name") === character2.getAttribute("name") && character1.id !== character2.id) {
+    if (character1.id !== character2.id && character1.getAttribute("name") === character2.getAttribute("name")) {
       character1.setAttribute("check", "found");
       character2.setAttribute("check", "found");
       return true;
@@ -76,9 +76,11 @@ class Game extends React.Component {
         {cards.map(card => {
           card.id = i + 1;
           i ++;
+          console.log(card)
           return(
             <div 
             className="image image-blank" 
+            id={card.id}
             name={card.name}
             style={ { background: `url(${card.img_url})`} } 
             check="false" 
