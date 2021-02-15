2a36846a2b23878548deeab59c56ff0f6425d869
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-15 15:46:49 -0500

    game component creates random deck with useeffect

--- src/components/hook-comp/Game.js
+++ src/components/hook-comp/Game.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
 import Card from './Card';
 import images from './images'
 
 const Game = () => {
   const [deck, setDeck] = useState([]);
 
-   const imageMap = () => images.map(image => {
-    console.log(image.name)
-  })
+  useEffect(() => {
+    let cards = _.sampleSize(images, 8);
+    let cards2 = cards;
+    cards = cards.concat(cards2).sort(() => Math.random() - 0.5);
+    setDeck(cards);
+  }, [])
 
   return (
     <div className="ui grid">
+      {console.log(deck)}
       <div className="four column row">
-        {imageMap()}
         <Card />
         <Card />
         <Card />
