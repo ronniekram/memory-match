b262e65eaf0f8a08a1000d63bdb6e78478021d4b
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-15 14:50:39 -0500

    card component skeleton

---
+++ src/components/hook-comp/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+      <div className="column">
+        <img src="https://react.semantic-ui.com/images/wireframe/image.png" className="ui image" alt="memory card"/>
+      </div>
+  )
+};
+
+export default Card;
