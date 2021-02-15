705714c58fe5debcfb86650ad88815ce86410a60
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-15 15:09:03 -0500

    images imported and logged in game component

--- src/components/hook-comp/Game.js
+++ src/components/hook-comp/Game.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
+import images from './images'
 
 const Game = () => {
+  const [deck, setDeck] = useState([]);
+
+   const imageMap = () => images.map(image => {
+    console.log(image.name)
+  })
+
   return (
     <div className="ui grid">
       <div className="four column row">
+        {imageMap()}
         <Card />
         <Card />
         <Card />
