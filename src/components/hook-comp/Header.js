67a9aa2d655316b05f940aaa0719c14ab6a46364
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-15 15:00:56 -0500

    updated header comp

--- src/components/hook-comp/Header.js
+++ src/components/hook-comp/Header.js
@@ -3,13 +3,14 @@ import React from 'react';
 const Header = ({ options, highScore, setOptions, setHighScore }) => {
   return (
     <div className="ui secondary pointing menu">
+      <h3 className="item">Garden Match</h3>
       <a className="active item">Home</a>
-      <a className="item">Messages</a>
-      <a className="item">Friends</a>
+      <a className="item">High Scores</a>
+      <a className="item">Difficulty</a>
       <div className="right menu">
-        <div className="item">
-            <p className="item"> High Score: {highScore}</p>
-        </div>
+        {/* <div className="item"> */}
+            <h3 className="item"> High Score: {highScore}</h3>
+        {/* </div> */}
       </div>
     </div>
   )
